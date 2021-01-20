05221db7d7bfbc388da2d6071e4b4ac950d5d75e
Marc Morera <yuhu@mmoreram.com>
2021-01-20 15:14:31 +0100

    Added php8 support

--- docker/wait-and-run.sh
+++ docker/wait-and-run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 
-while ! nc -z demo_mysql 3306;
+while ! nc -z driftphp_demo_mysql 3306;
 do
   echo "Waiting MYSQL. Slepping";
   sleep 1;
 done;
 echo "Connected to MYSQL!";
 
-while ! nc -z demo_amqp 5672;
+while ! nc -z driftphp_demo_amqp 5672;
 do
   echo "Waiting AMQP. Slepping";
   sleep 1;
